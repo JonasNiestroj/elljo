dc5c820a43fdac8fd8eb2acb492960b03e82029f
Jonas Niestroj <j.niestroj@protonmail.ch>
2021-07-16 17:00:33 +0200

    Move package & update readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # EllJo
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![npm version](https://badge.fury.io/js/%40elljo%2Fcompiler.svg)](https://badge.fury.io/js/%40elljo%2Fcompiler)
+
 EllJo is a web framework focused on performance. The compiler is written in golang and transforms your EllJo code into executable javascript.
 
 ## Getting started
--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,65 @@
-const request = require('request'),
-      tar = require('tar'),
-      mkdirp = require('mkdirp');
+const fetch = require('node-fetch');
+const tar = require('tar');
+const zlib = require('zlib');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const exec = require('child_process').exec;
+const path = require('path');
 
-function install() {
-    mkdirp.sync("./bin");
-    let ungz = zlib.createGunzip();
-    let untar = tar.Extract({ path: "./bin" });
+function getInstallationPath(callback) {
 
-    ungz.on('error', callback);
-    untar.on('error', callback);
+    // `npm bin` will output the path where binary files should be installed
+    exec("npm bin", function (err, stdout, stderr) {
 
-    // First we will Un-GZip, then we will untar. So once untar is completed,
-    // binary is downloaded into `binPath`. Verify the binary and call it good
-    untar.on('end', verifyAndPlaceBinary.bind(null, "jo", "./bin", callback));
+        let dir = null;
+        if (err || stderr || !stdout || stdout.length === 0) {
 
-    console.log("Downloading from URL: https://jo-compiler.s3.eu-central-1.amazonaws.com/jo_v0.0.0-SNAPSHOT-9e01fd1_darwin_amd64.tar.gz");
-    let req = request({ uri: "https://jo-compiler.s3.eu-central-1.amazonaws.com/jo_v0.0.0-SNAPSHOT-9e01fd1_darwin_amd64.tar.gz" });
-    req.on('error', callback.bind(null, "Error downloading from URL: https://jo-compiler.s3.eu-central-1.amazonaws.com/jo_v0.0.0-SNAPSHOT-9e01fd1_darwin_amd64.tar.gz"));
-    req.on('response', function (res) {
-        if (res.statusCode !== 200) return callback("Error downloading binary. HTTP Status Code: " + res.statusCode);
+            // We couldn't infer path from `npm bin`. Let's try to get it from
+            // Environment variables set by NPM when it runs.
+            // npm_config_prefix points to NPM's installation directory where `bin` folder is available
+            // Ex: /Users/foo/.nvm/versions/node/v4.3.0
+            let env = process.env;
+            if (env && env.npm_config_prefix) {
+                dir = path.join(env.npm_config_prefix, "bin");
+            }
+        } else {
+            dir = stdout.trim();
+        }
+
+        mkdirp.sync(dir);
+
+        callback(null, dir);
     });
 }
+
+function verifyAndPlaceBinary(binName, binPath, callback) {
+    fs.renameSync(path.join(binPath, binName), path.join(installationPath, binName));
+    getInstallationPath(function (err, installationPath) {
+        if (err) return callback("Error getting binary installation path from `npm bin`");
+
+        // Move the binary file
+        fs.renameSync(path.join(binPath, binName), path.join(installationPath, binName));
+
+        callback(null);
+    });
+}
+
+const install = async callback => {
+    mkdirp.sync("./bin");
+
+    let gunzip = zlib.createGunzip();
+    let untar = tar.x({ path: "./bin" });
+
+    // Pipe error to callback
+    gunzip.on('error', callback);
+    untar.on('error', callback);
+
+    // Copy file to bin directory
+    //untar.on('end', verifyAndPlaceBinary.bind(null, "jo", "./bin", callback));
+
+    let req = await fetch("https://jo-compiler.s3.eu-central-1.amazonaws.com/jo_v0.0.0-SNAPSHOT-9e01fd1_darwin_amd64.tar.gz");
+    req.body.pipe(gunzip).pipe(untar);
+};
 
 function uninstall(callback) {}
 
@@ -28,17 +67,16 @@ let argv = process.argv;
 if (argv && argv.length > 2) {
     let cmd = process.argv[2];
     if (cmd === "install") {
-        console.log("tet");
-        install(function (err) {
+        install(err => {
             if (err) {
                 console.error(err);
                 process.exit(1);
             } else {
-                console.log("INSTALLED");
                 process.exit(0);
             }
         });
     } else if (cmd === "uninstall") {
+        // TODO: Implement uninstall
         uninstall();
     }
 }
