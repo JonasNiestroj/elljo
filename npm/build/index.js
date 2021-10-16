48be0761e932ac36f6bae0d0d4583060c231f3c3
Jonas Niestroj <j.niestroj@protonmail.ch>
2021-10-16 20:23:49 +0200

    Update compiler npm package

--- npm/build/index.js
+++ npm/build/index.js
@@ -1,8 +1,19 @@
 const fetch = require('node-fetch');
 const tar = require('tar');
 const zlib = require('zlib');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const exec = require('child_process').exec;
+const path = require('path');
 
-const install = async (callback) => {
+const binPath = path.join(__dirname, '../.bin');
+
+function verifyAndPlaceBinary() {
+    console.log('copy to', binPath);
+    fs.copyFileSync('./elljo-compiler', binPath);
+}
+
+const install = async callback => {
     let gunzip = zlib.createGunzip();
     let untar = tar.x({ cwd: '../../.bin' });
 
@@ -10,9 +21,10 @@ const install = async (callback) => {
     gunzip.on('error', callback);
     untar.on('error', callback);
 
-    let req = await fetch(
-        'https://jo-compiler.s3.eu-central-1.amazonaws.com/elljo_0.0.0-SNAPSHOT-acf1211_darwin_amd64.tar.gz'
-    );
+    // Copy file to bin directory
+    // untar.on('end', verifyAndPlaceBinary);
+
+    let req = await fetch('https://jo-compiler.s3.eu-central-1.amazonaws.com/elljo_0.0.0-SNAPSHOT-acf1211_darwin_amd64.tar.gz');
     req.body.pipe(gunzip).pipe(untar);
 };
 
@@ -22,7 +34,7 @@ let argv = process.argv;
 if (argv && argv.length > 2) {
     let cmd = process.argv[2];
     if (cmd === 'install') {
-        install((err) => {
+        install(err => {
             if (err) {
                 console.error(err);
                 process.exit(1);
